31dd63789f2590e57745123d6b18634a3b68c4a4
AleksandrBausher <bausheraleksandr@gmail.com>
2023-05-27 11:36:56 -0400

    completed home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,13 @@
 import React from "react";
 import { Typewriter } from "react-simple-typewriter";
+import html from "../images/html.png"
+import css from "../images/css.png"
+import react from "../images/react.png"
+import js from "../images/js.png"
+import node from "../images/node.png"
+import me from "../images/f_logo.png"
+
+import "./Home.css"
 
 const Home = () => {
   return (
@@ -32,6 +40,45 @@ const Home = () => {
               tempus semper. Mauris vitae libero a risus feugiat egestas sit
               amet non orci.
             </p>
+            <div className="me_btn d_flex">
+              <div className="col_1">
+                <h4>FIND WITH ME</h4>
+                <div className="button">
+                  <button className="btn_shadow">
+                    <i class="fab fa-facebook-f"></i>
+                  </button>
+                  <button className="btn_shadow">
+                    <i class="fab fa-instagram"></i>
+                  </button>
+                  <button className="btn_shadow">
+                    <i class="fab fa-linkedin-in"></i>
+                  </button>
+                </div>
+              </div>
+              <div className="col_1">
+                <h4>BEST SKILL ON</h4>
+                <button className="btn_shadow">
+                  <img src={html} alt="" />
+                </button>
+                <button className="btn_shadow">
+                  <img src={css} alt="" />
+                </button>
+                <button className="btn_shadow">
+                  <img src={js} alt="" />
+                </button>
+                <button className="btn_shadow">
+                  <img src={react} alt="" />
+                </button>
+                <button className="btn_shadow">
+                  <img src={node} alt="" />
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="right">
+            <div className="right_img">
+              <img src={me} alt="" />
+            </div>
           </div>
         </div>
       </section>
