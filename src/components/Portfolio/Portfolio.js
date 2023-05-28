0d347e3b779776edd1d38de8cc3ef460b6700283
AleksandrBausher <bausheraleksandr@gmail.com>
2023-05-28 19:05:30 -0400

    designed portfoio completed and tweek the theme a little bit

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,28 +1,29 @@
-import React from "react";
-import "./Portfolio.css";
-import Card from "./Card";
+import React from "react"
+import "./Portfolio.css"
+import Card from "./Card"
 
 const Portfolio = () => {
   return (
     <>
-      <section className="Portfolio top" id="portfolio">
-        <div className="container">
-          <div className="heading text-center ">
+      <section className='Portfolio top' id='portfolio'>
+        <div className='container'>
+          <div className='heading text-center '>
             <h4>VISIT MY PORTFOLIO AND KEEP YOUR FEEDBACK</h4>
             <h1>My Portfolio</h1>
           </div>
-          <div className="content grid">
-            <Card
-              image="./image/html.png"
-              category="Project"
-              totalLike="200"
-              title="The Weather Dashboard"
-            />
+
+          <div className='content grid'>
+               <Card image="" category="Personal Project" totalLike="100" title="The Film Reel" />
+               <Card image="" category="Personal Project" totalLike="100" title="Rent My Stuff" />
+               <Card image="" category="Personal Project" totalLike="100" title="JATE" />
+               <Card image="" category="Personal Project" totalLike="100" title="Work Day Schedule" />
+               <Card image="" category="Personal Project" totalLike="100" title="Modern Weather Dashboard" />
+               <Card image="" category="Personal Project" totalLike="100" title="Personal Note Taker" />
           </div>
         </div>
       </section>
     </>
-  );
-};
+  )
+}
 
-export default Portfolio;
+export default Portfolio
